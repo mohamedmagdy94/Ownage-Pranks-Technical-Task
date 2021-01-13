4ac79f7c03df664c4a0b53fb5ec1cbb3fec81e7b
Bassel Hossam <basselhossam.a@gmail.com>
2021-01-13 05:51:46 +0200

    apply reactstrap

--- FrontEnd/src/App/App.js
+++ FrontEnd/src/App/App.js
@@ -1,13 +1,32 @@
 import './App.scss';
 import * as React from "react";
 import Header from "../components/Header/Header";
+import {Button, Col, Container, Jumbotron, Row} from 'reactstrap';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
+            <div>
                 <Header />
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>Welcome to React</h1>
+                                <p className="text-primary">
+                                    test primary text
+                                </p>
+                                <p className="text-secondary">
+                                    test secondary text
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
             </div>
+            // <div className="App">
+            //     <Header />
+            // </div>
         );
     }
 }
--- FrontEnd/src/components/Header/Header.js
+++ FrontEnd/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import logo from './logo.png';
 import './Header.scss';
 import * as React from "react";
+import {Navbar, NavbarBrand} from "reactstrap";
 
 class Header extends React.Component {
     render() {
         return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
+            <Navbar color="dark" dark>
+                <NavbarBrand href="/" className="mx-auto"><img src={logo} className="App-logo" alt="logo"/></NavbarBrand>
+            </Navbar>
         );
     }
 }
